4a7ba92282c7c363e76fa1944b5c0d1caedae4a4
pknowledge <jamesblssings@gmail.com>
2024-09-26 09:29:47 +0300

    changes in the javascript

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';  // Replace 'Grace Hopper' with any name you'd like
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
